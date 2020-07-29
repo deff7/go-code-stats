8160cc5d48cb032e8432c74579dbc5a0d942757a
Sergey Malenkov <smalenkov@ozon.ru>
2020-07-29 15:13:57 +0300

    Experiments with go/types: ast walker

--- main.go
+++ main.go
@@ -2,13 +2,35 @@ package main
 
 import (
 	"fmt"
+	"go/ast"
+	"go/importer"
 	"go/parser"
 	"go/token"
+	"go/types"
 	"log"
 )
 
+func walkScope(scope *types.Scope) {
+	for _, name := range scope.Names() {
+		obj := scope.Lookup(name)
+		if obj == nil {
+			log.Printf("no such name: %v", name)
+			continue
+		}
+
+		switch v := obj.(type) {
+		case *types.Func:
+			walkScope(v.Scope())
+		case *types.Var:
+			log.Println(v.Name(), " -- ", v.Type().String())
+		default:
+			log.Printf("unknown %#v", v)
+		}
+	}
+}
+
 func main() {
-	filename := "./test/example_go"
+	filename := "./test/simple_go"
 	mode := parser.Mode(0)
 	fset := token.NewFileSet()
 
@@ -23,4 +45,17 @@ func main() {
 		}
 		fmt.Println(imp.Path.Value)
 	}
+
+	conf := types.Config{Importer: importer.Default()}
+
+	pkg, err := conf.Check("cmd/hello", fset, []*ast.File{f}, nil)
+	if err != nil {
+		log.Fatal(err) // type error
+	}
+
+	fmt.Printf("Package  %q\n", pkg.Path())
+	fmt.Printf("Name:    %s\n", pkg.Name())
+	fmt.Printf("Imports: %s\n", pkg.Imports())
+	fmt.Printf("Scope:   %s\n", pkg.Scope())
+	walkScope(pkg.Scope())
 }
